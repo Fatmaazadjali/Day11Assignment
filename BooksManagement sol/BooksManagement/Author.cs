78f086637a7051fcd24d9f76f74b5ea102734fe6
Fatmazadjali <maathermohammed77@gmail.com>
2023-11-29 18:02:37 +0400

    first step

---
+++ BooksManagement sol/BooksManagement/Author.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksManagement
+{
+	internal class Author
+	{
+	   string AuthName;
+		string Nationality;
+		//int BirthYear;
+
+		public Author() { } //Constructor
+		public Author(string AuthorName) { 
+		
+		  this.AuthName = AuthorName;
+			this.Nationality = Nationalityy;
+			//this.BirthYear = BirthY;
+		}
+		
+		//public int BirthYear
+		//{
+		//	get { return BirthYear; }
+		//	set { AuthName = value.ToString(); }
+		//}
+		
+		//public int displayAuthorInfo1(int BirthYear) => $"Birth year {this.BirthYear} ";
+
+		public string AuthorName
+		{
+
+			get { return this.AuthName; }
+			set { this.AuthName = value; }
+
+		}
+
+		public string Nationalityy
+		{
+
+			get { return this.Nationality; }
+				
+
+		}
+
+		//public int BirthY
+		//{
+
+		//	get { return this.BirthYear; }
+		//	set { this.BirthYear = value; }
+		//}
+
+		public string displayAuthorInfo(string AuthName, string Nationality) => $"Author Name {this.AuthName}   \n Nationality: {Nationalityy}  ";
+
+
+	}
+}
---
+++ BooksManagement sol/BooksManagement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BooksManagement
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Books & Author System!");
+			Author author = new Author { 
+				
+				AuthorName = "J.K. Rowling",
+				Nationalityy = "British"
+			
+			};
+			
+            Console.WriteLine(author.AuthorName, author.Nationalityy);
+          
+
+
+
+        }
+	}
+}
